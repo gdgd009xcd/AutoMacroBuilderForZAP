034ac8c981226f842f23d11f58a45bd673e03bd8
gdgd009xcd <patsz.g1owq@gmail.com>
2020-09-30 22:57:15 +0900

    Bugfix: collect historyReferences from Site Map's node tree and add these to MacroBuilder.

--- addOns/automacrobuilder/src/main/java/org/zaproxy/zap/extension/automacrobuilder/zap/PopupMenuAdd2MacroBuilder.java
+++ addOns/automacrobuilder/src/main/java/org/zaproxy/zap/extension/automacrobuilder/zap/PopupMenuAdd2MacroBuilder.java
@@ -19,18 +19,28 @@
  */
 package org.zaproxy.zap.extension.automacrobuilder.zap;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.parosproxy.paros.model.HistoryReference;
+import org.parosproxy.paros.model.Model;
+import org.parosproxy.paros.model.SiteNode;
 import org.zaproxy.zap.extension.automacrobuilder.*;
 import org.zaproxy.zap.extension.automacrobuilder.generated.LangSelectDialog;
 import org.zaproxy.zap.extension.automacrobuilder.generated.MacroBuilderUI;
 import org.zaproxy.zap.extension.automacrobuilder.mdepend.ClientDependMessageContainer;
-import org.zaproxy.zap.view.popup.PopupMenuItemHistoryReferenceContainer;
+import org.zaproxy.zap.model.StructuralNode;
+import org.zaproxy.zap.model.StructuralSiteNode;
+import org.zaproxy.zap.model.Target;
+import org.zaproxy.zap.view.popup.PopupMenuItemSiteNodeContainer;
 
-public class PopupMenuAdd2MacroBuilder extends PopupMenuItemHistoryReferenceContainer
+public class PopupMenuAdd2MacroBuilder extends PopupMenuItemSiteNodeContainer
         implements InterfaceLangOKNG {
 
+    private static final org.apache.logging.log4j.Logger LOGGER4J =
+            org.apache.logging.log4j.LogManager.getLogger();
     private static final long serialVersionUID = 1L;
     private LangSelectDialog langdialog = null;
     private ParmGenMacroTrace pmt = null;
@@ -46,12 +56,99 @@ public class PopupMenuAdd2MacroBuilder extends PopupMenuItemHistoryReferenceCont
         langdialog = new LangSelectDialog(null, this, Encode.ISO_8859_1, true);
     }
 
-    @Override
-    public void performAction(HistoryReference href) {}
+    private String getIndentString(int i) {
+        String spc = "";
+        while (i-- > 0) {
+            spc += " ";
+        }
+        return spc;
+    }
+
+    /**
+     * traverse Site node tree and colloect historyreferences of leaf node.
+     *
+     * @param node
+     * @param level
+     * @param historyrefs
+     */
+    private void collectLeafNodes(
+            StructuralNode node, int level, List<HistoryReference> historyrefs) {
+        if (node.isLeaf()) {
+            HistoryReference href = node.getHistoryReference();
+            if (href != null) {
+                historyrefs.add(href);
+                LOGGER4J.debug(
+                        getIndentString(level)
+                                + level
+                                + ":leaf name:"
+                                + node.getName()
+                                + " href:"
+                                + href.getURI().toString());
+            } else {
+                LOGGER4J.debug(
+                        getIndentString(level)
+                                + level
+                                + ":leaf name:"
+                                + node.getName()
+                                + " href:null");
+            }
+        } else {
+            LOGGER4J.debug(getIndentString(level) + level + ":NO leaf name:" + node.getName());
+        }
+        Iterator<StructuralNode> it = node.getChildIterator();
+        if (it != null) {
+            while (it.hasNext()) {
+                collectLeafNodes(it.next(), level + 1, historyrefs);
+            }
+        }
+    }
 
     @Override
-    protected void performHistoryReferenceActions(List<HistoryReference> hrefs) {
-        this.hrefs = hrefs;
+    protected void performAction(SiteNode siteNode) {
+        LOGGER4J.debug("performAction called.");
+        Target target = new Target(siteNode);
+        if (target.getStartNodes() != null) {
+            List<StructuralNode> nodes = target.getStartNodes();
+            if (nodes.size() == 1 && nodes.get(0).isRoot()) { // selected root node.
+                LOGGER4J.debug("ROOT:" + nodes.get(0).getName());
+
+                Iterator<StructuralNode> iter = nodes.get(0).getChildIterator();
+                while (iter.hasNext()) {
+                    StructuralNode node = iter.next();
+                    collectLeafNodes(node, 0, this.hrefs);
+                }
+            } else {
+                for (StructuralNode node : nodes) {
+                    collectLeafNodes(node, 0, this.hrefs);
+                }
+            }
+        } else { // Nodes in Context scope.
+            List<SiteNode> nodes = Collections.emptyList();
+            if (target.isInScopeOnly()) {
+                nodes = Model.getSingleton().getSession().getTopNodesInScopeFromSiteTree();
+            } else if (target.getContext() != null) {
+                nodes = target.getContext().getTopNodesInContextFromSiteTree();
+            }
+            // Loop through all of the top nodes containing children
+            for (SiteNode snode : nodes) {
+                StructuralNode node = new StructuralSiteNode(snode);
+                collectLeafNodes(node, 0, this.hrefs);
+            }
+        }
+    }
+
+    @Override
+    protected void performHistoryReferenceActions(List<HistoryReference> srchrefs) {
+        LOGGER4J.debug("performHistoryReferenceActions called.");
+        this.hrefs = new ArrayList<>();
+
+        // collect historyReferences of leaf node from StructuralSiteNodes(Site node tree).
+        for (HistoryReference historyReference : srchrefs) {
+            if (historyReference != null) {
+                this.performAction(historyReference);
+            }
+        }
+
         this.hrefs.stream()
                 .forEach(
                         href -> {
@@ -59,7 +156,7 @@ public class PopupMenuAdd2MacroBuilder extends PopupMenuItemHistoryReferenceCont
                         });
 
         this.listprr =
-                hrefs.stream()
+                this.hrefs.stream()
                         .map(
                                 href ->
                                         new PRequestResponse(
